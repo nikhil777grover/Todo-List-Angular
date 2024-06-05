0d92b3591627300405e5426433bcaace0edbda5a
Nikhil <nikhil777grover@gmail.com>
2024-06-06 00:51:37 +0530

    adding Todo mdel and base code for Todo component

---
+++ src/app/Components/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent{
+  todos : Todo[]
+  constructor()
+  {
+    this.todos = [
+      {
+        sno : 1,
+        title : "Title 1",
+        description : "Description 1",
+        active : false
+      },
+      {
+        sno : 2,
+        title : "Title 2",
+        description : "Description 2",
+        active : false
+      },
+      {
+        sno : 3,
+        title : "Title 3",
+        description : "Description 3",
+        active : false
+      }
+    ]
+  }
+}
